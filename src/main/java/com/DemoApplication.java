a60a3041b26e00c6aea9962204f771c78e5a2161
developpeurcurieux <developpeurcurieux@gmail.com>
2017-08-27 11:51:57 +0200

    form en cours.

--- src/main/java/com/DemoApplication.java
+++ src/main/java/com/DemoApplication.java
@@ -13,20 +13,53 @@ import org.springframework.data.domain.PageRequest;
 
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) throws ParseException {
-		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
-                EtudiantRepository er = ctx.getBean(EtudiantRepository.class);
-                
-                DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-                
-                er.save(new Etudiant("Ahmad", df.parse("1988-11-10"), "ahmed@gmail.com", "ahmed.jpg"));
-                er.save(new Etudiant("Martin", df.parse("1988-10-10"), "martin@gmail.com", "martin.jpg"));
-                er.save(new Etudiant("Jamil", df.parse("1988-09-10"), "jamil@gmail.com", "jamil.jpg"));
-                
-                Page<Etudiant> etds = er.chercherEtudiants("%J%", new PageRequest(0, 4));
-                etds.forEach(e -> System.out.println(e.getNom()));
-                
-        }
+    
+    public static void main(String[] args) throws ParseException {
+        ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+        EtudiantRepository er = ctx.getBean(EtudiantRepository.class);
         
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        
+        er.save(new Etudiant("Ahmad", df.parse("1988-11-10"), "ahmed@gmail.com", "ahmed.jpg"));
+        er.save(new Etudiant("Martin", df.parse("1988-10-10"), "martin@gmail.com", "martin.jpg"));
+        er.save(new Etudiant("Jamil", df.parse("1988-09-10"), "jamil@gmail.com", "jamil.jpg"));
+        er.save(new Etudiant("Ahmad", df.parse("1988-11-10"), "ahmed@gmail.com", "ahmed.jpg"));
+        er.save(new Etudiant("Martin", df.parse("1988-10-10"), "martin@gmail.com", "martin.jpg"));
+        er.save(new Etudiant("Jamil", df.parse("1988-09-10"), "jamil@gmail.com", "jamil.jpg"));
+        er.save(new Etudiant("Ahmad", df.parse("1988-11-10"), "ahmed@gmail.com", "ahmed.jpg"));
+        er.save(new Etudiant("Martin", df.parse("1988-10-10"), "martin@gmail.com", "martin.jpg"));
+        er.save(new Etudiant("Jamil", df.parse("1988-09-10"), "jamil@gmail.com", "jamil.jpg"));
+        er.save(new Etudiant("Ahmad", df.parse("1988-11-10"), "ahmed@gmail.com", "ahmed.jpg"));
+        er.save(new Etudiant("Martin", df.parse("1988-10-10"), "martin@gmail.com", "martin.jpg"));
+        er.save(new Etudiant("Jamil", df.parse("1988-09-10"), "jamil@gmail.com", "jamil.jpg"));
+        er.save(new Etudiant("Ahmad", df.parse("1988-11-10"), "ahmed@gmail.com", "ahmed.jpg"));
+        er.save(new Etudiant("Martin", df.parse("1988-10-10"), "martin@gmail.com", "martin.jpg"));
+        er.save(new Etudiant("Jamil", df.parse("1988-09-10"), "jamil@gmail.com", "jamil.jpg"));
+        er.save(new Etudiant("Ahmad", df.parse("1988-11-10"), "ahmed@gmail.com", "ahmed.jpg"));
+        er.save(new Etudiant("Martin", df.parse("1988-10-10"), "martin@gmail.com", "martin.jpg"));
+        er.save(new Etudiant("Jamil", df.parse("1988-09-10"), "jamil@gmail.com", "jamil.jpg"));
+        er.save(new Etudiant("Ahmad", df.parse("1988-11-10"), "ahmed@gmail.com", "ahmed.jpg"));
+        er.save(new Etudiant("Martin", df.parse("1988-10-10"), "martin@gmail.com", "martin.jpg"));
+        er.save(new Etudiant("Jamil", df.parse("1988-09-10"), "jamil@gmail.com", "jamil.jpg"));
+        er.save(new Etudiant("Ahmad", df.parse("1988-11-10"), "ahmed@gmail.com", "ahmed.jpg"));
+        er.save(new Etudiant("Martin", df.parse("1988-10-10"), "martin@gmail.com", "martin.jpg"));
+        er.save(new Etudiant("Jamil", df.parse("1988-09-10"), "jamil@gmail.com", "jamil.jpg"));
+        er.save(new Etudiant("Ahmad", df.parse("1988-11-10"), "ahmed@gmail.com", "ahmed.jpg"));
+        er.save(new Etudiant("Martin", df.parse("1988-10-10"), "martin@gmail.com", "martin.jpg"));
+        er.save(new Etudiant("Jamil", df.parse("1988-09-10"), "jamil@gmail.com", "jamil.jpg"));
+        er.save(new Etudiant("Ahmad", df.parse("1988-11-10"), "ahmed@gmail.com", "ahmed.jpg"));
+        er.save(new Etudiant("Martin", df.parse("1988-10-10"), "martin@gmail.com", "martin.jpg"));
+        er.save(new Etudiant("Jamil", df.parse("1988-09-10"), "jamil@gmail.com", "jamil.jpg"));
+        
+        
+        
+        
+        
+        
+        
+        Page<Etudiant> etds = er.chercherEtudiants("%J%", new PageRequest(0, 4));
+        etds.forEach(e -> System.out.println(e.getNom()));
+        
+    }
+    
 }
--- src/main/java/com/controllers/EtudiantController.java
+++ src/main/java/com/controllers/EtudiantController.java
@@ -7,18 +7,39 @@
 package com.controllers;
 
 import com.dao.EtudiantRepository;
+import com.entities.Etudiant;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
+@RequestMapping(value="/Etudiant")
 public class EtudiantController {
     @Autowired
     private EtudiantRepository etudiantRepository;
     
     @RequestMapping(value="/index")
-    public String index() {
+    public String index(Model model, 
+            @RequestParam(name="page", defaultValue="0")int p, 
+            @RequestParam(name="motCle", defaultValue="")String mc) {
+        
+        Page<Etudiant> pageEtudiants = etudiantRepository.chercherEtudiants("%"+mc+"%", new PageRequest(p, 5));
+        int pagesCount = pageEtudiants.getTotalPages();
+        int[] pages = new int[pagesCount];
+        
+        for(int i = 0; i < pagesCount; i++) {
+            pages[i] = i;
+        }
+        
+        model.addAttribute("pages", pages);
+        model.addAttribute("pageCourante", p);
+        model.addAttribute("pageEtudiants", pageEtudiants);
+        model.addAttribute("motCle", mc);
         return "etudiants";
     }
 }
