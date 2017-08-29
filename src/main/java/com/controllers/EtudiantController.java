acff929804c9bf47ff9d7360876702d3555dba99
developpeurcurieux <developpeurcurieux@gmail.com>
2017-08-29 11:56:12 +0200

    finalize: ok

--- src/main/java/com/controllers/EtudiantController.java
+++ src/main/java/com/controllers/EtudiantController.java
@@ -9,17 +9,23 @@ package com.controllers;
 import com.dao.EtudiantRepository;
 import com.entities.Etudiant;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import javax.validation.Valid;
+import org.apache.commons.compress.utils.IOUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 
@@ -28,6 +34,9 @@ import org.springframework.web.multipart.MultipartFile;
 public class EtudiantController { 
     @Autowired
     private EtudiantRepository etudiantRepository;
+    
+    @Value("${dir.images}")
+    private String imageDir;
     
     @RequestMapping(value="/index")
     public String index(Model model, 
@@ -58,20 +67,67 @@ public class EtudiantController {
     @RequestMapping(value="/saveEtudiant", method=RequestMethod.POST)
     public String save(@Valid Etudiant etudiant, 
             BindingResult bindingResult,
-            @RequestParam(name="id")MultipartFile file) throws IOException {
+            @RequestParam(name="picture")MultipartFile file) throws IOException {
         if(bindingResult.hasErrors()) {
             return "FormEtudiant";
         }
         
+        if( !(file.isEmpty())) {
+            etudiant.setPhoto(file.getOriginalFilename());    
+        }
+        
+      
+        
         if( !(file.isEmpty() )) {
-            System.out.println("--------------------------");
-            System.out.println(file.getOriginalFilename());
             etudiant.setPhoto(file.getOriginalFilename());
-            file.transferTo(new File("C:/Users/cdi213/sco"));
+             etudiantRepository.save(etudiant);
+            file.transferTo(new File(imageDir + etudiant.getId()));
             
         }
+        return "redirect:index";
+    }
+    
+    @RequestMapping(value="/getPhoto", produces=MediaType.IMAGE_JPEG_VALUE)
+    @ResponseBody() // on ne renvoit pas le nom de la vue, mais une photo donnees binaires
+    public byte[] getPhoto(Long id) throws Exception {
+        File f = new File(imageDir + id);
+        return IOUtils.toByteArray(new FileInputStream(f));
+    }
+    
+    
+    @RequestMapping(value="/supprimer")
+    public String supprimer(@RequestParam("id")Long id) {
+        etudiantRepository.delete(id);
+        return"redirect:index";
+    }
+    
+    @RequestMapping(value="/edit")
+    public String edit(Model model, @RequestParam("id")Long id) {
+        Etudiant et = etudiantRepository.getOne(id);
+        model.addAttribute("etudiant", et);
+        return "EditEtudiant";
+    }
+    
+    @RequestMapping(value="/updateEtudiant", method=RequestMethod.POST)
+    public String update(@Valid Etudiant etudiant, 
+            BindingResult bindingResult,
+            @RequestParam(name="picture")MultipartFile file) throws IOException {
+        if(bindingResult.hasErrors()) {
+            return "EditEtudiant";
+        }
+        
+        if( !(file.isEmpty())) {
+            etudiant.setPhoto(file.getOriginalFilename());    
+        }
+        
         etudiantRepository.save(etudiant);
         
+        if( !(file.isEmpty() )) {
+            etudiant.setPhoto(file.getOriginalFilename());
+            etudiantRepository.save(etudiant);
+            file.transferTo(new File(imageDir + etudiant.getId()));
+            
+        }
         return "redirect:index";
     }
 }
--- src/main/resources/static/js/myScript.js
+++ src/main/resources/static/js/myScript.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $("#donnees").click(function(){
-        $("#hg").toggle(1000);
+        $("#hg").toggle("slow");
     });
 });
